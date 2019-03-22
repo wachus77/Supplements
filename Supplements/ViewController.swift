6b8eda59a36824ce9f05ed33162f46228c091343
Tomasz Iwaszek <tiwaszek@concisesoftware.com>
2019-03-22 14:25:22 +0100

    project init

---
+++ Supplements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Supplements
+//
+//  Created by Tomasz Iwaszek on 3/22/19.
+//  Copyright © 2019 matchusolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
