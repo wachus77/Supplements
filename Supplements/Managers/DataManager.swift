3fd289402f183fdacb1eb449a4e2920eae597c9c
Tomasz Iwaszek <wachus77@gmail.com>
2019-04-12 14:29:43 +0200

    added data manager class

---
+++ Supplements/Managers/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  DataManager.swift
+//  Supplements
+//
+//  Created by Tomasz Iwaszek on 4/12/19.
+//  Copyright © 2019 matchusolutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol DataManager: class {
+
+}
+
+class DataSourceManager: DataManager {
+    
+    required init() {
+
+    }
+}
